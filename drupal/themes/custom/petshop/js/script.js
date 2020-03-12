a8c5bcab5c514b334547e1bd839b585bc2a4825c
m3lod13 <manzoni.elodie@gmail.com>
2020-03-12 12:33:14 +0100

    page detail

--- drupal/themes/custom/petshop/js/script.js
+++ drupal/themes/custom/petshop/js/script.js
@@ -1,6 +1,15 @@
 (function($){
 	$( document ).ready(function() {
 
+		$('.filters > .btn-primary').click(function(){
+			
 
+			if($('.filters > .btn-primary').attr('aria-expanded') == 'true'){
+				$(this).children('h4').html('Filtres <i class="fas fa-plus-square"></i>')
+			} else {
+				$(this).children('h4').html('Filtres <i class="fas fa-minus-square"></i>')		
+			}
+
+		});
 	}) ;
 });
--- integration/js/script.js
+++ integration/js/script.js
@@ -1,4 +1,13 @@
 $( document ).ready(function() {
+	
+	$('.filters > .btn-primary').click(function(){
+		
 
+		if($('.filters > .btn-primary').attr('aria-expanded') == 'true'){
+			$(this).children('h4').html('Filtres <i class="fas fa-plus-square"></i>')
+		} else {
+			$(this).children('h4').html('Filtres <i class="fas fa-minus-square"></i>')		
+		}
 
+	});
 }) ;
